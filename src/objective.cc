70df4e9e779e204ed1d62492d8b406eeefd696b5
eunnie12 <eunnieverse@gmail.com>
2015-05-27 16:14:11 -0400

    changed date

--- src/objective.cc
+++ src/objective.cc
@@ -3,7 +3,7 @@
  * compute Torque FOM
  * for a given BEM matrix, OPFT matrix, frequency, and PARMMatrix
  * created 2015.03
- * last updated on 2015.05.22
+ * last updated on 2015.05.27
  *--------------------------------------------------------------*/
 #include <stdio.h>
 #include <math.h>
