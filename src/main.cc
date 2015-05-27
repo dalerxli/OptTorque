e71f00ee6dfb306c6b14c9db58fe8b1a986d94bf
eunnie12 <eunnieverse@gmail.com>
2015-05-27 17:46:43 -0400

    .

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,8 @@
 /*---------------------------------------------------------------
  * main.cc  
- * runs the nlopt routine using objective function 
+ * contains examples of NLOPT in C 
  * created 2015.05.21
- * last updated on 2015.05.22
+ * last updated on 2015.05.27
  *--------------------------------------------------------------*/
 #include <stdio.h>
 #include <math.h>
@@ -14,10 +14,6 @@
 #define II cdouble(0.0,1.0)
 #define MAXSTR 100 
 using namespace std;
-// strategy 
-// main function will be calling all other functions. 
-// objective function gets a vector of size n. 
-double objective(unsigned n, const double *x, double *grad) 
 //---------------------------------------------------------------//
 double myfunc(unsigned n, const double *x, double *grad, void *my_func_data)
 {
@@ -45,50 +41,11 @@ double myconstraint(unsigned n, const double *x, double *grad, void *data)
  }
 //---------------------------------------------------------------//
 int main(int argc, char *argv[])
-  /// Objective 
-  /// 
-  /// READ In:
-  ///    parameter matrix PARMMatrix 
-  ///    BEM matrix A, LU Factorized
-  ///    PFT matrix Q 
 {
-  /*--------------------------------------------------------------*/
-  /*- process options  -------------------------------------------*/
-  /*--------------------------------------------------------------*/
-  /// Input arguments should be filenames. (pointers) 
-  char *GeoFile=0; 
-  char *PARMMatrixFile=0;
-  char *BEMMatrixFile=0;
-  char *PFTMatrixFile=0;
-  char *LogLevel=0;
-  /* name    type   #args  max_instances  storage  count  description*/
-  OptStruct OSArray[]=
-    { 
-      {"geometry",   PA_STRING,  1, 1, (void *)&GeoFile, 0,  ".scuffgeo file"},
-      {"PARMMatrix", PA_STRING,  1, 1, (void *)&PARMMatrixFile, 0, "VParameters file"},
-      {"BEMMatrix",  PA_STRING,  1, 1, (void *)&BEMMatrixFile, 0,  "BEM Matrix datafile"},
-      {"PFTMatrix",  PA_STRING,  1, 1, (void *)&PFTMatrixFile, 0,  "PFT Matrix datafile"},
-      {"LogLevel",   PA_STRING,  1, 1, (void *)&LogLevel, 0, "none | terse | verbose | verbose2"},
-      {0,0,0,0,0,0,0}
-    };
-  ProcessOptions(argc, argv, OSArray);
-  // if (GeoFile==0)
-  //   OSUsage(argv[0],OSArray,"--geometry option is mandatory");
-  // if (PARMMatrixFile==0)
-  //   OSUsage(argv[0],OSArray,"--PARMMatrix option is mandatory");
-  // if (BEMMatrixFile==0)
-  //  OSUsage(argv[0],OSArray,"--BEMMatrix option is mandatory");
-  RWGGeometry *G = new RWGGeometry(GeoFile);
-  HMatrix *PARMMatrix = new HMatrix(PARMMatrixFile, LHM_TEXT);
-  ShowPARMMatirx(PARMMatrix); 
-  HMatrix *M = new HMatrix(BEMMatrixFile, LHM_TEXT);
-  //A->LUFactorize(); //(already done)
-  HMatrix *Q = new HMatrix(PFTMatrixFile, LHM_TEXT);
-  objective(G, PARMMatrix, M, Q); 
-  
-  delete G, PARMMatrix, M, Q; 
-}
+
  //  nlopt_set_upper_bounds; 
  //  my_constraint_data data[2] = {{},{}}; 
  //  nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-8);
  //  nlopt_add_inequality_constraint(opt, myconstraint, &data[1], 1e-8);
+
+}
--- src/objective.cc
+++ src/objective.cc
@@ -24,7 +24,10 @@ using namespace scuff;
 double GetIntegratedIntensity(RWGGeometry *G, int SurfaceIndex, 
                               HVector *RHSVector);
 void ShowPARMMatrix(HMatrix* PARMMatrix); 
-double objective(unsigned n, double *x, double *grad);
+double objective_Qabs(unsigned n, double *x, double *grad);
+double objective_QTZ(unsigned n, double *x, double *grad) ;
+double objective_QFZ(unsigned n, double *x, double *grad) ;
+
 //---------------------------------------------------------------//
 int main()
 {
@@ -39,15 +42,17 @@ int main()
       x[l*5+3] = 0.0; //ai
       x[l*5+4] = 0.0; //bi
     }
-  objective(NUML*5, x, grad); 
+
+  objective_QFZ(NUML*5, x, grad); 
 
       // char MatFileName[100];
       // snprintf(MatFileName, 100, "Mat_Cadj.dat");  
       // Cadj->ExportToText(MatFileName,"--separate,"); 
 
 }
+
 //---------------------------------------------------------------//
-double objective(unsigned n, double *x, double *grad) 
+double objective_Qabs(unsigned n, double *x, double *grad) 
 {
   cdouble Omega = 10.471975511965978; 
   //---------------------------------------------------------------//
@@ -65,14 +70,15 @@ double objective(unsigned n, double *x, double *grad)
   printf("NR = %i\n",NR); 
   HMatrix *QabsOPFT = new HMatrix(HDF5File, LHM_HDF5,"QabsOPFT");
   if (QabsOPFT->ErrMsg) ErrExit(QabsOPFT->ErrMsg);
-  HMatrix *QFZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QFZOPFT");
-  if (QFZOPFT->ErrMsg) ErrExit(QFZOPFT->ErrMsg);
-  HMatrix *QTZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QTZOPFT");
-  if (QTZOPFT->ErrMsg) ErrExit(QTZOPFT->ErrMsg);
-  printf("size of QabsOPFT is : %i by %i\n",QabsOPFT->NR,QabsOPFT->NC);   
+  // HMatrix *QFZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QFZOPFT");
+  // if (QFZOPFT->ErrMsg) ErrExit(QFZOPFT->ErrMsg);
+  // HMatrix *QTZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QTZOPFT");
+  // if (QTZOPFT->ErrMsg) ErrExit(QTZOPFT->ErrMsg);
+  printf("size of Q is : %i by %i\n",QabsOPFT->NR,QabsOPFT->NC);   
   Log(" Successfully imported all matrices...\n");
   //---------------------------------------------------------------//
-  // initialize RHS vectors and IF from double *x. 
+
+  // --- initialize RHS vectors and IF from double *x. 
   Log("  Assembling the RHS vector...");
   HVector* KN1=new HVector(NR,LHM_COMPLEX); 
   HMatrix* PMatrix=new HMatrix(NUML,6,LHM_REAL); 
@@ -113,8 +119,7 @@ double objective(unsigned n, double *x, double *grad)
   HMatrix* C    = new HMatrix(NR,1,KN->RealComplex, LHM_NORMAL, KN->ZV); 
   // --- choose Q 
   HMatrix* Q = new HMatrix(NR,NR,LHM_COMPLEX);
-  //  Q->Copy(QTZOPFT); // Choose as torque matrix 
-  Q->Copy(QabsOPtorque matrix 
+  //  Q->Copy(QTZOPFT); // Choose as torque matrix  
   Q->Copy(QabsOPFT); // Choose as torque matrix 
 
   // --- compute FOM = Cconj*Q*C , where C=KN and Q=QTZOPFT
@@ -157,6 +162,179 @@ double objective(unsigned n, double *x, double *grad)
       }    
       delete Cadj; 
     }
+  delete KN1, PMatrix, KN, RHS, TEMPMAT, FOM, IF, C, Q, Cconj; 
+  return FOM->GetEntryD(0,0); 
+}//end objective 
+
+//---------------------------------------------------------------//
+double objective_QTZ(unsigned n, double *x, double *grad) 
+{
+  cdouble Omega = 10.471975511965978; 
+  //---------------------------------------------------------------//
+  char FileBase[MAXSTR], GeoFile[MAXSTR], HDF5File[MAXSTR];
+  snprintf(FileBase,MAXSTR,"N3_400nm_Mesh60nm"); 
+  snprintf(GeoFile,MAXSTR,"%s.scuffgeo",FileBase);  
+  snprintf(HDF5File,MAXSTR,"%s.HDF5",FileBase ); 
+  // I want the GeoFile and HDF5File to search for *.scuffgeo and *.HDF5 in the dir. 
+  RWGGeometry *G = new RWGGeometry(GeoFile);
+  Log(" Created RWGGeometry G...\n");
+  //---------------------------------------------------------------//
+  HMatrix *MLU = new HMatrix(HDF5File, LHM_HDF5, "MLU");
+  if (MLU->ErrMsg) ErrExit(MLU->ErrMsg);
+  int NR = MLU->NR; // number of rows 
+  printf("NR = %i\n",NR); 
+  // HMatrix *QabsOPFT = new HMatrix(HDF5File, LHM_HDF5,"QabsOPFT");
+  // if (QabsOPFT->ErrMsg) ErrExit(QabsOPFT->ErrMsg);
+  // HMatrix *QFZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QFZOPFT");
+  // if (QFZOPFT->ErrMsg) ErrExit(QFZOPFT->ErrMsg);
+  HMatrix *QTZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QTZOPFT");
+  if (QTZOPFT->ErrMsg) ErrExit(QTZOPFT->ErrMsg);
+  printf("size of Q is : %i by %i\n",QTZOPFT->NR,QTZOPFT->NC);   
+  Log(" Successfully imported all matrices...\n");
+  //---------------------------------------------------------------//
+ 
+
+  // --- initialize RHS vectors and IF from double *x. 
+  Log("  Assembling the RHS vector...");
+  HVector* KN1=new HVector(NR,LHM_COMPLEX); 
+  HMatrix* PMatrix=new HMatrix(NUML,6,LHM_REAL); 
+  double cnorm[NUML]; 
+  // KN1=G->AllocateRHSVector();
+  for (int l; l<NUML; l++){
+    // for each l, compute cnorm[l]
+    // IncField *IF1 = new VBeam(l,x[l*5],x[l*5+1],x[l*5+2],x[l*5+3],x[l*5+4]); 
+    IncField *IF1 = new VBeam(l,x[l*5]);     // initialize with VBeam = 1*M; 
+    G->AssembleRHSVector(Omega, IF1, KN1);   // KN1 is formed here
+    cnorm[l]=GetIntegratedIntensity(G, 0, KN1); // get cnorm for each (l,aIn)
+
+    // create a big PMatrix with each row corresponding to one mode 
+    // normalize ar,br,ai,bi
+    PMatrix->SetEntry(l,0,l); 
+    PMatrix->SetEntry(l,1,x[l*5]); 
+    PMatrix->SetEntry(l,2,x[l*5+1]/cnorm[l]); 
+    PMatrix->SetEntry(l,3,x[l*5+2]/cnorm[l]); 
+    PMatrix->SetEntry(l,4,x[l*5+3]/cnorm[l]); 
+    PMatrix->SetEntry(l,5,x[l*5+4]/cnorm[l]); 
+    delete IF1; 
+  }
+  //ShowPARMMatrix(PMatrix);
+
+  // --- allocate necessary vectors and matrices
+  HVector* KN=new HVector(NR,LHM_COMPLEX); 
+  HVector* RHS=new HVector(NR,LHM_COMPLEX); 
+
+  HMatrix* TEMPMAT = new HMatrix(NR,1,LHM_COMPLEX); 
+  HMatrix* FOM = new HMatrix(1,1,LHM_COMPLEX); 
+  // --- create IncField from the normalized PMatrix 
+  IncField* IF=new VBeam(PMatrix);
+  // --- compute RHS, Solve for C=KN
+  G->AssembleRHSVector(Omega, IF, KN); 
+  RHS->Copy(KN);
+  MLU->LUFactorize(); 
+  MLU->LUSolve(KN);// solved KN. 
+  HMatrix* C    = new HMatrix(NR,1,KN->RealComplex, LHM_NORMAL, KN->ZV); 
+  // --- choose Q 
+  HMatrix* Q = new HMatrix(NR,NR,LHM_COMPLEX);
+  Q->Copy(QTZOPFT); // Choose as torque matrix  
+  //Q->Copy(QabsOPFT); // Choose as torque matrix 
+
+  // --- compute FOM = Cconj*Q*C , where C=KN and Q=QTZOPFT
+  HMatrix* Cconj = new HMatrix(NR,1,LHM_COMPLEX); 
+  Cconj->Copy(C); 
+  Cconj->Adjoint(); // row vector for Adjoint(C)
+  Q->Multiply(C,TEMPMAT); // TEMPMAT= Q*C
+  Cconj->Multiply(TEMPMAT,FOM); // FOM= Cconj^T*Q*C 
+  printf("Computed FOM = (%e)+(%e)i.\n",
+         real(FOM->GetEntry(0,0)),imag(FOM->GetEntry(0,0)));
+  // --- if gradient value is required, use adjoint method 
+  // --- M*Cadj = transpose(QPFT)*conj(C)  
+
+  delete KN1, PMatrix, KN, RHS, TEMPMAT, FOM, IF, C, Q, Cconj; 
+  return FOM->GetEntryD(0,0); 
+}//end objective 
+
+//---------------------------------------------------------------//
+double objective_QFZ(unsigned n, double *x, double *grad) 
+{
+  cdouble Omega = 10.471975511965978; 
+  //---------------------------------------------------------------//
+  char FileBase[MAXSTR], GeoFile[MAXSTR], HDF5File[MAXSTR];
+  snprintf(FileBase,MAXSTR,"N3_400nm_Mesh60nm"); 
+  snprintf(GeoFile,MAXSTR,"%s.scuffgeo",FileBase);  
+  snprintf(HDF5File,MAXSTR,"%s.HDF5",FileBase ); 
+  // I want the GeoFile and HDF5File to search for *.scuffgeo and *.HDF5 in the dir. 
+  RWGGeometry *G = new RWGGeometry(GeoFile);
+  Log(" Created RWGGeometry G...\n");
+  //---------------------------------------------------------------//
+  HMatrix *MLU = new HMatrix(HDF5File, LHM_HDF5, "MLU");
+  if (MLU->ErrMsg) ErrExit(MLU->ErrMsg);
+  int NR = MLU->NR; // number of rows 
+  printf("NR = %i\n",NR); 
+  // HMatrix *QabsOPFT = new HMatrix(HDF5File, LHM_HDF5,"QabsOPFT");
+  // if (QabsOPFT->ErrMsg) ErrExit(QabsOPFT->ErrMsg);
+  HMatrix *QFZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QFZOPFT");
+  if (QFZOPFT->ErrMsg) ErrExit(QFZOPFT->ErrMsg);
+  // HMatrix *QTZOPFT = new HMatrix(HDF5File, LHM_HDF5, "QTZOPFT");
+  // if (QTZOPFT->ErrMsg) ErrExit(QTZOPFT->ErrMsg);
+  printf("size of Q is : %i by %i\n",QFZOPFT->NR,QFZOPFT->NC);   
+  Log(" Successfully imported all matrices...\n");
+  //---------------------------------------------------------------//
+ 
+  // --- initialize RHS vectors and IF from double *x. 
+  Log("  Assembling the RHS vector...");
+  HVector* KN1=new HVector(NR,LHM_COMPLEX); 
+  HMatrix* PMatrix=new HMatrix(NUML,6,LHM_REAL); 
+  double cnorm[NUML]; 
+  // KN1=G->AllocateRHSVector();
+  for (int l; l<NUML; l++){
+    // for each l, compute cnorm[l]
+    // IncField *IF1 = new VBeam(l,x[l*5],x[l*5+1],x[l*5+2],x[l*5+3],x[l*5+4]); 
+    IncField *IF1 = new VBeam(l,x[l*5]);     // initialize with VBeam = 1*M; 
+    G->AssembleRHSVector(Omega, IF1, KN1);   // KN1 is formed here
+    cnorm[l]=GetIntegratedIntensity(G, 0, KN1); // get cnorm for each (l,aIn)
+
+    // create a big PMatrix with each row corresponding to one mode 
+    // normalize ar,br,ai,bi
+    PMatrix->SetEntry(l,0,l); 
+    PMatrix->SetEntry(l,1,x[l*5]); 
+    PMatrix->SetEntry(l,2,x[l*5+1]/cnorm[l]); 
+    PMatrix->SetEntry(l,3,x[l*5+2]/cnorm[l]); 
+    PMatrix->SetEntry(l,4,x[l*5+3]/cnorm[l]); 
+    PMatrix->SetEntry(l,5,x[l*5+4]/cnorm[l]); 
+    delete IF1; 
+  }
+  //ShowPARMMatrix(PMatrix);
+
+  // --- allocate necessary vectors and matrices
+  HVector* KN=new HVector(NR,LHM_COMPLEX); 
+  HVector* RHS=new HVector(NR,LHM_COMPLEX); 
+
+  HMatrix* TEMPMAT = new HMatrix(NR,1,LHM_COMPLEX); 
+  HMatrix* FOM = new HMatrix(1,1,LHM_COMPLEX); 
+  // --- create IncField from the normalized PMatrix 
+  IncField* IF=new VBeam(PMatrix);
+  // --- compute RHS, Solve for C=KN
+  G->AssembleRHSVector(Omega, IF, KN); 
+  RHS->Copy(KN);
+  MLU->LUFactorize(); 
+  MLU->LUSolve(KN);// solved KN. 
+  HMatrix* C    = new HMatrix(NR,1,KN->RealComplex, LHM_NORMAL, KN->ZV); 
+  // --- choose Q 
+  HMatrix* Q = new HMatrix(NR,NR,LHM_COMPLEX);
+  //  Q->Copy(QTZOPFT); // Choose as torque matrix  
+  Q->Copy(QFZOPFT); // Choose as torque matrix 
+
+  // --- compute FOM = Cconj*Q*C , where C=KN and Q=QTZOPFT
+  HMatrix* Cconj = new HMatrix(NR,1,LHM_COMPLEX); 
+  Cconj->Copy(C); 
+  Cconj->Adjoint(); // row vector for Adjoint(C)
+  Q->Multiply(C,TEMPMAT); // TEMPMAT= Q*C
+  Cconj->Multiply(TEMPMAT,FOM); // FOM= Cconj^T*Q*C 
+  printf("Computed FOM = (%e)+(%e)i.\n",
+         real(FOM->GetEntry(0,0)),imag(FOM->GetEntry(0,0)));
+  // --- if gradient value is required, use adjoint method 
+  // --- M*Cadj = transpose(QPFT)*conj(C)  
+
   delete KN1, PMatrix, KN, RHS, TEMPMAT, FOM, IF, C, Q, Cconj; 
   return FOM->GetEntryD(0,0); 
 }//end objective 
